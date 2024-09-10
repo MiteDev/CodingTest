5d4cec13b3edeb2a2d091d69e00d97ef83971247
Dong <74600676+MiteDev@users.noreply.github.com>
2024-09-10 22:49:34 +0900

    [Bronze I] Title: 팰린드롬수, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
@@ -8,21 +8,9 @@ const input = fs.readFileSync(file_path).toString().trim();
  */
 const solution = (input) => {
     const arr = input.replace(/\r/g, '').split('\n');
-    
     // 마지막 요소는 의미없으니 제거
     arr.pop();
-
-    const halfs = arr.map(el => Math.floor(el.length / 2));
-
-    const result = arr.map((el, idx) => {
-        const lastNum = el.length % 2 ? halfs[idx] + 1 : halfs[idx];
-        const first = el.substring(0, halfs[idx]);
-        const last = el.substring(lastNum, el.length).split('').reverse().join('');
-
-        return first === last ? 'yes' : 'no';
-    });
-
-    const answer = result.join('\n');
+    const answer = arr.map(line => line === line.split('').reverse().join('') ? 'yes' : 'no').join('\n');
      
     return answer;
 }
