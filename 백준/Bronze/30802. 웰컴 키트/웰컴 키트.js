7217015f6e0171a052f5885de01523c33e1039a8
Dong <74600676+MiteDev@users.noreply.github.com>
2024-09-20 13:04:20 +0900

    [Bronze III] Title: 웰컴 키트, Time: 88 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.js
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.js
@@ -9,30 +9,15 @@ const input = fs.readFileSync(file_path).toString().trim();
 const solution = (input) => {
     const [entrant, size_str, shirt_pen] = input.replace(/\r/g, '').split('\n');
     const n = Number(entrant);
-    const size = size_str.split(' ').map(el => Number(el));
-    const [t, p] = shirt_pen.split(' ').map(el => Number(el));
-    let t_count = 0;
+    const size = size_str.split(' ').map(Number);
+    const [t, p] = shirt_pen.split(' ').map(Number);
 
-    for(let i = 0; i < size.length; i++) {
-        let temp = 1;
-        let temp_t = t;
-        while(true) {
-            if(!size[i]) break;
-
-            const _t = temp_t * temp;
-
-            if(_t >= size[i]) {
-                t_count += temp;
-                break;
-            }
-            temp++;
-        }
-    }
+    const result = size.reduce((acc ,cur) => acc += Math.ceil(cur / t), 0);
 
     const bundle_p = Math.floor(n / p);
     const ea = n % p;
 
-    const answer = `${t_count}\n${bundle_p} ${ea}`;
+    const answer = `${result}\n${bundle_p} ${ea}`;
 
     return answer;
 }
