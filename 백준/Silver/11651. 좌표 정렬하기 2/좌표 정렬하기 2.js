16a0f21eec9a78726de2e0ee77d0fe055b918037
Dong <74600676+MiteDev@users.noreply.github.com>
2024-09-29 09:47:27 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 400 ms, Memory: 56096 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const file_path =
+  process.platform === "linux" ? "dev/stdin" : `${__dirname}/input.txt`;
+const input = fs.readFileSync(file_path).toString().trim();
+
+/**
+ * @param {string} input
+ * @returns
+ */
+const solution = (input) => {
+  const [_, ...coordi] = input.replace(/\r/g, "").split("\n");
+  const arr = coordi.map((el) => el.split(" ").map(Number));
+  const answer = arr
+    .sort((a, b) => {
+      if (a[1] - b[1] === 0) {
+        return a[0] - b[0];
+      }
+      return a[1] - b[1];
+    })
+    .map(([x, y]) => `${x.toString()} ${y.toString()}`)
+    .join("\n");
+  return answer;
+};
+
+console.log(solution(input));
