ae6d92a74ed1fcb1e8d9c717591005410b4f94b0
Dong <74600676+MiteDev@users.noreply.github.com>
2024-10-02 09:49:44 +0900

    [Silver IV] Title: 스택, Time: 164 ms, Memory: 12592 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.js
+++ 백준/Silver/10828. 스택/스택.js
@@ -14,10 +14,10 @@ const solution = (input) => {
 
   arr.forEach((line) => {
     if (line.includes("push")) stack.push(parseInt(line.split(" ")[1]));
-    else if (line === "pop") answer.push(stack.length ? stack.pop() : -1);
-    else if (line === "size") answer.push(stack.length);
-    else if (line === "empty") answer.push(stack.length ? 0 : 1);
-    else if (line === "top") answer.push(stack.length ? stack.at(-1) : -1);
+    if (line === "pop") answer.push(stack.length ? stack.pop() : -1);
+    if (line === "size") answer.push(stack.length);
+    if (line === "empty") answer.push(stack.length ? 0 : 1);
+    if (line === "top") answer.push(stack.length ? stack.at(-1) : -1);
   });
 
   return answer.join("\n");
